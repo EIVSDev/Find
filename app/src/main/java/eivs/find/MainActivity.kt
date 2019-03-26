2d6d64e218771f375b3be7a96cbb152818477aa8
Slava <eivsdnepr@gmail.com>
2019-03-26 20:34:53 +0200

    Create project

---
+++ app/src/main/java/eivs/find/MainActivity.kt
@@ -0,0 +1,145 @@
+package eivs.find
+
+import android.Manifest
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.iid.FirebaseInstanceId
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import eivs.find.model.User
+import eivs.find.utils.Common
+import io.paperdb.Paper
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val MY_REQUEST_CODE = 7117
+    }
+
+    lateinit var user_information: DatabaseReference
+    lateinit var providers: List<AuthUI.IdpConfig>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        //Init
+        Paper.init(this)
+
+        //Init Firebase
+        user_information = FirebaseDatabase.getInstance().getReference(Common.USER_INFORMATION)
+
+        //Init Provider
+        providers = Arrays.asList<AuthUI.IdpConfig>(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        //Request permission
+        Dexter.withActivity(this)
+            .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            .withListener(object: PermissionListener{
+                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                    showSignInOptions()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                    Toast.makeText(this@MainActivity,"You must accept this permission",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }).check()
+
+    }
+
+    private fun showSignInOptions() {
+        startActivityForResult(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build(),MY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MY_REQUEST_CODE)
+        {
+            val firebaseUser = FirebaseAuth.getInstance().currentUser
+            //Check if user exist on database
+                 user_information.orderByKey()
+                .equalTo(firebaseUser!!.uid)
+                     .addListenerForSingleValueEvent(object:ValueEventListener{
+                         override fun onCancelled(p0: DatabaseError) {
+                             Toast.makeText(this@MainActivity,"onCancelled" + p0,Toast.LENGTH_SHORT).show()
+                         }
+
+                         override fun onDataChange(dataSnapshot: DataSnapshot)
+                         {
+                             Toast.makeText(this@MainActivity,"onDataChange",Toast.LENGTH_SHORT).show()
+                           if (dataSnapshot.value == null)
+                           {
+                               //User not exist
+                               if(!dataSnapshot.child(firebaseUser.uid).exists())
+                               {
+                                   Common.loggedUser = User(firebaseUser.uid,firebaseUser.email!!)
+                                   //Add user to database
+                                   user_information.child(Common.loggedUser.uid!!)
+                                       .setValue(Common.loggedUser)
+                               }
+                           }
+                             else
+                           {
+                               //User available
+                               Common.loggedUser = dataSnapshot.child(firebaseUser.uid)
+                                   .getValue(User::class.java)!!
+                           }
+
+                             //Save UID to storage to update location from killed mode
+                             Paper.book().write(Common.USER_UID_SAVE_KEY,Common.loggedUser.uid)
+                             updateToken(firebaseUser)
+                             setupUI()
+                         }
+
+                     })
+        }
+
+    }
+
+    private fun setupUI() {
+        //After all done !Navigation Home
+        startActivity(Intent(this@MainActivity,HomeActivity::class.java))
+        finish()
+
+    }
+
+    private fun updateToken(firebaseUser: FirebaseUser?){
+        val tokens = FirebaseDatabase.getInstance()
+            .getReference(Common.TOKENS)
+
+        //Get Token
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnSuccessListener { instanceIdResult ->
+                tokens.child(firebaseUser!!.uid)
+                    .setValue(instanceIdResult.token)
+            }.addOnFailureListener { e ->
+                Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+            }
+    }
+}
---
+++ app/src/main/java/eivs/find/utils/Common.kt
@@ -0,0 +1,10 @@
+package eivs.find.utils
+
+import eivs.find.model.User
+
+object Common {
+    val USER_INFORMATION: String = "UserInformation"
+    lateinit var loggedUser: User
+    val TOKENS: String = "Tokens"
+    val USER_UID_SAVE_KEY: String = "SAVE_KEY"
+}
